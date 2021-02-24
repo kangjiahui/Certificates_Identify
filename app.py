cf29cdf15a87c7ce77b0d3b21dde68c376a4bad9
kangjiahui <kangjiahui@enn.cn>
2021-02-24 10:45:45 +0800

    flask fix

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import json
 import yaml
 from gevent import pywsgi
+import base64
 
 app = Flask(__name__)
 CORS(app, resources=r'/*')
@@ -13,32 +14,37 @@ with open("confs/host.yaml", "r", encoding='UTF-8') as f:
 
 
 # 根据config文件重置空result字典
-@app.route('/reset', methods=["GET"])
+@app.route('/reset', methods=["POST"])
 def reset():
-    result = api.ocr_reset()
-    return result
+    # result = api.ocr_reset()
+    # return result
+    print(123)
+    return '0'
 
 
 # 提取单张证件信息
 @app.route('/ocr', methods=["POST"])
 def ocr():
-    input_data = json.loads(request.data)
-    image_base64 = input_data["image"]
-    result = api.ocr_recog_one(image_base64)
-    return result
+    if request.form['img_type'] == 'pic':
+        input_data = request.files
+        result = api.ocr_recog_one(base64.b64encode(input_data["user_image"].read()))
+        return result
+    else:
+        return api.ocr_recog_one(request.form['user_image'])
 
 
 # 现场人脸核验
 @app.route('/face', methods=["POST"])
 def face():
-    input_data = json.loads(request.data)
-    image_base64 = input_data["image"]
-    result = api.match_face_ocr(image_base64)
-    return result
+    if request.form['img_type'] == 'pic':
+        input_data = request.files
+        return api.match_face_ocr(base64.b64encode(input_data["user_image"].read()))
+    else:
+        return api.match_face_ocr(request.form['user_image'])
 
 
 # 证件信息比对
-@app.route('/match', methods=["GET"])
+@app.route('/match', methods=["POST"])
 def match():
     result = api.info_match()
     return result
